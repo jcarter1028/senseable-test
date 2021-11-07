fc9dffaf9a51fba185ebdf162db9f6aa484467e1
jcarter1028 <jcarter.1028@gmail.com>
2021-11-07 22:32:32 +0000

    Update to menu button

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,1 +1,10 @@
 console.log("Menu");
+
+let menuOpen = false;
+const clickSubmenu = document.getElementById("#click-submenu");
+
+document.getElementById("#menu-button").addEventListener("click", function () {
+  console.log("Handle menu button clicked");
+  menuOpen = !menuOpen;
+  clickSubmenu.className = `menu-click-submenu ${menuOpen ? "" : "hidden"}`;
+});
